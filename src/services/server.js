21a2f6ba2b67d4b5d7d164b9c56884d4b359acaf
Mateus Bezerra <83496671+mateusasb@users.noreply.github.com>
2024-10-07 00:45:43 -0300

    Fix

--- src/services/server.js
+++ src/services/server.js
@@ -2,7 +2,7 @@ const io = require('socket.io')(5000, {
     cors: {
         origin: ["http://localhost:3000"]
     }
-})
+});
 
 io.on("connection", (socket) => {
     const roomExists = (roomName) => {
